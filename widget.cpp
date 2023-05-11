7479fa2e3ee516117295ee379e564ef7fa0dacf9
Alexander <sanjzamik10000@mail.ru>
2023-05-11 20:10:56 +0700

    memory allocate exception final fix

--- widget.cpp
+++ widget.cpp
@@ -5,22 +5,9 @@ Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
     // Обработка исключения при неуспешном выделении памяти
-    try {
-        ui = new Ui::Widget;
-        if (ui == nullptr) {
-            throw std::bad_alloc();
-        };
-    }
-    catch (const std::bad_alloc&) {
-        QMessageBox meesageBox(QMessageBox::Information,
-                               "Счетчик",
-                               "Ошибка: невозможно выделить память. Пожалуйста, перезапустите приложение!",
-                               QMessageBox::Ok);
-        // Запускаем диалоговое окно сообщения msgBox и блокирует дальнейшее выполнение
-        // программы до тех пор, пока пользователь не закроет это окно
-        meesageBox.exec();
-        exit(0);
-    };
+    ui = new Ui::Widget;
+    if (ui == nullptr)
+    {throw std::bad_alloc();}
 
     // Создаем элементы пользовательского интерфейса, которые были размещены на форме
     ui->setupUi(this);
